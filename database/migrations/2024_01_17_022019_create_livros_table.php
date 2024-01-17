b5fb526ba53bcaca48ef34f25756707fd71ba80d
José Henrique Cabianchi Orlandi <joseorlandi13@gmail.com>
2024-01-16 23:26:24 -0300

    Ajuste na migration Livros

--- database/migrations/2024_01_17_022019_create_livros_table.php
+++ database/migrations/2024_01_17_022019_create_livros_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('livros', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('nome_livro');
+            $table->timestamp('dt_inclusao', 0)->nullable();
+            $table->timestamp('dt_alteracao', 0)->nullable();
         });
     }
 
