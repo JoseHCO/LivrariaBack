6923fa90df1feb38dfcca5fc6916e148e018e9ce
José Henrique Cabianchi Orlandi <joseorlandi13@gmail.com>
2024-01-16 23:27:47 -0300

    Models Usuario/Livro populada

--- app/Models/Livro.php
+++ app/Models/Livro.php
@@ -2,10 +2,53 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Livro extends Model
 {
     use HasFactory;
+    
+    protected $table = 'livros';
+    const CREATED_AT = 'dt_inclusao';
+    const UPDATED_AT = 'dt_alteração';
+
+    protected $fillable = [
+        'nome_livro'
+    ];
+
+    public static function criarLivro($dados)
+    {
+        return self::create($dados);
+    }
+
+    public static function obterLivros()
+    {
+        return self::all();
+    }
+
+    public static function obterLivroId($livroId)
+    {
+        return self::find($livroId);
+    }
+
+    public static function atualizarLivro($livroId, $dados)
+    {
+        $livro = self::find($livroId);
+        if ($livro) {
+            $livro->update($dados);
+            return $livro;
+        }
+        return null;
+    }
+
+    public static function excluirLivro($livroId)
+    {
+        $livro = self::find($livroId);
+        if ($livro) {
+            $livro->delete();
+            return true;
+        }
+        return false;
+    }
 }
--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -2,10 +2,54 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Usuario extends Model
 {
     use HasFactory;
+    
+    protected $table = 'usuarios';
+    const CREATED_AT = 'dt_inclusao';
+    const UPDATED_AT = 'dt_alteração';
+    
+    protected $fillable = [
+        'nome',
+        'cpf'
+    ];
+
+    public static function criarUsuario($dados)
+    {
+        return self::crate($dados);
+    }
+
+    public static function obterUsuarios()
+    {
+        return self::all();
+    }
+
+    public static function obterUsuarioId($usuarioId)
+    {
+        return self::find($usuarioId);
+    }
+
+    public static function atualizarUsuario($usuarioId, $dados)
+    {
+        $usuario = self::find($usuarioId);
+        if ($usuario) {
+            $usuario->update($dados);
+            return $usuario;
+        }
+        return null;
+    }
+
+    public static function excluirUsuario($usuarioId)
+    {
+        $usuario = self::find($usuarioId);
+        if ($usuario) {
+            $usuario->delete();
+            return true;
+        }
+        return false;
+    }
 }
