733ae9526b18b6dc7d35eaccebe32f849dbc2133
José Henrique Cabianchi Orlandi <joseorlandi13@gmail.com>
2024-01-16 23:30:30 -0300

    Requests criadas e populadas

---
+++ app/Http/Requests/StoreUsuarioRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUsuarioRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    protected function prepareForValidation(): void 
+    {
+        $this->merge([
+            'cpf' => preg_replace('/\D/', '', $this->cpf)
+        ]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules()
+    {
+        return [
+            'nome' => 'required|string|max:255',
+            'cpf' => 'required|string|unique:usuarios,cpf|size:11'
+        ];
+    }
+}
